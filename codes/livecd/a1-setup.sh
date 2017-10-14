d19d894d839fcd0582f4cb6d85f4645a90bdf685
azhongbo <azhongbo@gmail.com>
2017-10-14 19:07:28 +0800

    update

--- codes/livecd/a1-setup.sh
+++ codes/livecd/a1-setup.sh
@@ -31,35 +31,23 @@ mkdir /livecd
 mount /dev/nbd0p1 /livecd
 
 mkdir -p /livecd/isomount /livecd/isonew/{squashfs,cd,custom}
+
+
 cd /livecd
-
 mount -o loop,ro $MateISO isomount
-
 rsync --exclude=/casper/filesystem.squashfs -a isomount/ isonew/cd/
-
 modprobe squashfs
-
 mount -t squashfs -o loop isomount/casper/filesystem.squashfs isonew/squashfs
-
 rsync -a isonew/squashfs/ isonew/custom/
-
 umount isonew/squashfs
 umount isomount
 
 
+Soft='/media/ubuntu-mate/ext4/tools'
+cp $Soft/geckodriver                       /livecd/isonew/custom//usr/local/bin/
+cp $Soft/phantomjs                         /livecd/isonew/custom/usr/bin/
+cp $Soft/VMware-Workstation-Full-14.bundle /livecd/isonew/custom/tmp/
+cp "$runPath"/all-install.sh               /livecd/isonew/custom/root/
+
 cd /livecd
-#echo nameserver 8.8.8.8     > /tmp/resolv.conf
-#echo nameserver 168.95.1.1 >> /tmp/resolv.conf
-
-#echo deb http://archive.ubuntu.com/ubuntu/ zesty main restricted universe multiverse             > /tmp/sources.list
-#echo deb http://security.ubuntu.com/ubuntu/ zesty-security main restricted universe multiverse  >> /tmp/sources.list
-#echo deb http://archive.ubuntu.com/ubuntu/ zesty-updates main restricted universe multiverse    >> /tmp/sources.list
-#echo deb https://download.sublimetext.com/ apt/stable/                                           > /tmp/sublime-text.list
-
-#cp /tmp/resolv.conf           /livecd/isonew/custom/etc/
-#cp /tmp/sources.list          /livecd/isonew/custom/etc/apt/
-#cp /tmp/sublime-text.list     /livecd/isonew/custom/etc/apt/sources.list.d/
-
-cp "$runPath"/all-install.sh  /livecd/isonew/custom/root/
-
 chroot isonew/custom
--- codes/livecd/all-install.sh
+++ codes/livecd/all-install.sh
@@ -46,14 +46,10 @@ echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select tr
 apt-get -y install ttf-mscorefonts-installer
 apt-get -y install ubuntu-restricted-extras
 
-
 apt-get -y install apache2 php7.0 php7.0-sqlite3 sqlite3 
 
 wget -O /tmp/vscode.deb https://go.microsoft.com/fwlink/?LinkID=760868
 dpkg -i /tmp/vscode.deb
-
-wget -O /tmp/phantomjs.tar.bz2  https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
-
 
 
 #debconf-set-selections <<< 'mysql-server mysql-server/root_password password 1234'
