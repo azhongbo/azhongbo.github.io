175e4725485034aac94fb726cf064f7351ae1ee7
azhongbo <azhongbo@gmail.com>
2021-12-11 07:36:16 +0800

    test

--- data_tf2_basic.js
+++ data_tf2_basic.js
@@ -101,51 +101,149 @@ chkData(`
 # https://github.com/aladdinpersson/Machine-Learning-Collection/blob/master/ML/TensorFlow/Basics/tutorial3-neuralnetwork.py
 
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.keras import layers, utils
+from tensorflow.keras.layers import Dropout
 from tensorflow.keras.datasets import mnist
+from tensorflow.keras import utils
 
 physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0],True)
 
 
-(x_train,y_train) , (x_test, y_test ) = mnist.load_data()
-x_train = x_train.reshape(-1,28*28).astype('float32') / 255.0
-x_test  = x_test.reshape(-1,28*28).astype("float32") / 255.0
+###### 載入 mnist #######
 
-## Sequential mode ##
+(train_images, train_labels) , (test_images, test_labels ) = mnist.load_data()
+
+# 正規化
+train_normalization = x_train_images.reshape(-1,28*28).astype('float32') / 255.0
+test_normalization  = x_test_images.reshape(-1,28*28).astype("float32")  / 255.0
+
+# OneHot (後面 model.fit 會自動處理 onehot )
+# train_label_onehot = utils.to_categorical(train_labels)
+# test_label_onehot  = utils.to_categorical(test_labels)
+
+
+###### 顯示圖片 #######
+def plot_image(image):
+    fig = plt.gcf()
+    fig.set_size_inches(2,2)
+    plt.imshow(image, cmap="binary")
+    plt.show
+
+def plot_images_labels_prediction(images,labels,prediction,idx,num=10):
+    fig = plt.gcf()
+    fig.set_size_inches(12,14)
+    if num>25: num=25
+    for i in range(0, num):
+        ax=plt.subplot(5, 5, 1+i)
+        ax.imshow(images[idx], cmap="binary")
+        title= f"label: {str(labels[idx])}"
+        if len(prediction)>0:
+            title+=f" prediction: {str(prediction[idx])}"
+        ax.set_title(title,fontsize=10)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        idx+=1
+    plt.show()
+
+plot_images_labels_prediction(train_images, train_labels,[],10,20)
+
+
+###### 建立模型 #######
+
+## Sequential mode 方法一 ##
 # model = keras.Sequential([
 #     keras.Input(shape=(28*28)),
-#     layers.Dense(512, activation='relu'),
-#     layers.Dense(256, activation='relu'),
-#     layers.Dense(10),
+#     layers.Dense(512, activation="relu"),
+#     Dropout(0.5),
+#     layers.Dense(256, activation="relu"),
+#     Dropout(0.5),
+#     layers.Dense(10,  activation="softmax"),
 # ])
 
-# model = keras.Sequential()
-# model.add(keras.Input(shape=(784)) )
-# model.add(layers.Dense(512,activation="relu"))
-# model.add(layers.Dense(256,activation="relu"))
-# model.add(layers.Dense(10,activation="relu"))
+## Sequential mode 方法二 ##
+model = keras.Sequential()
+model.add(keras.Input(shape=(784)) )
+model.add(layers.Dense(1000,activation="relu"))
+model.add(Dropout(0.5))
+model.add(layers.Dense(256,activation="relu"))
+model.add(Dropout(0.5))
+model.add(layers.Dense(10,activation="softmax"))
 
-## functional mode ##
-inputs = keras.Input(shape=[784])
-x = layers.Dense(512,activation="relu",name="h1")(inputs)
-x = layers.Dense(256,activation="relu",name="h2")(x)
-outputs = layers.Dense(10,activation="softmax")(x)
-model = keras.Model(inputs=inputs,outputs=outputs)
+## functional mode 方法三 ##
+# inputs = keras.Input(shape=[784])
+# x = layers.Dense(512,activation="relu",name="h1")(inputs)
+# x = layers.Dropout(0.5)(x)
+# x = layers.Dense(256,activation="relu",name="h2")(x)
+# x = layers.Dropout(0.5)(x)
+# outputs = layers.Dense(10,activation="softmax")(x)
+# model = keras.Model(inputs=inputs,outputs=outputs)
 
 print(model.summary())
 
+
+###### 開始訓練 建立損失函數 優化器 ######
 model.compile(
     loss=keras.losses.SparseCategoricalCrossentropy(from_logits=False),
     optimizer=keras.optimizers.Adam(lr=0.001),
     metrics=["accuracy"],
 )
 
-model.fit(x_train, y_train, batch_size=32, epochs=5, verbose=2)
-model.evaluate(x_test,y_test, batch_size=32, verbose=2)
+train_history = model.fit(train_normalization,   # 填入 train 資料 (已正規化)
+                          train_labels,          # 填入 標籤 (不需要 OneHot)
+                          batch_size=32, 
+                          epochs=10, 
+                          verbose=2,             # 顯示訓練過程
+                          validation_split=0.2)  # validation_split 80%訓練 20%驗證
+
+
+
+
+###### 繪製正確率 錯誤率 ######
+import matplotlib.pyplot as plt
+def show_train_history(train_history):
+    plt.plot(train_history.history['accuracy'])
+    plt.plot(train_history.history['loss'])
+    plt.plot(train_history.history['val_accuracy'])
+    plt.plot(train_history.history['val_loss'])
+    plt.title("Train History")
+    plt.ylabel('accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['accuracy','loss','val_accuracy','val_loss'],loc='upper right')
+    plt.show()
+
+show_train_history(train_history)
+
+
+###### 預測準確率 ######
+scores = model.evaluate( test_normalization, test_labels, batch_size=32, verbose=2)
+print(f"accuracy={scores[1]}")
+
+
+###### 顯示測驗結果 ######
+plot_images_labels_prediction(test_images,test_labels,prediction,idx=340)
+
+###### 建立混淆矩陣 ######
+import pandas as pd
+pd.crosstab(test_labels, 
+            prediction, 
+            rownames=['test_labels'],
+            colnames=['prediction'] )
+
+
+###### 真實與預測的 dataframe ######
+df = pd.DataFrame({'test_labels': test_labels, 'prediction': prediction})
+df[:10]
+
+###### dataframe 找出預測錯誤 ######
+df[(df.test_labels==5)&(df.prediction!=5)]
+
+###### 顯示錯誤的圖片 ######
+plot_images_labels_prediction(test_images,test_labels,prediction,idx=4360,num=1)
+
 `)
 
 
