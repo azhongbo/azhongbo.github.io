4f52e1c3f85c7dad4206e37101c38c684c58572b
azhongbo <32501317+azhongbo@users.noreply.github.com>
2023-10-04 09:24:47 +0800

    test

--- fabric/Fabric01.java
+++ fabric/Fabric01.java
@@ -8,15 +8,15 @@
 
 // src\main\java\net\kaupenjoe\tutorialmod\item\ModItemGroups.java
 package net.kaupenjoe.tutorialmod.item;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup; // 這是從 Fabric API 導入的 FabricItemGroup 類。此類允許您創建自定義項目組。
 import net.kaupenjoe.tutorialmod.TutorialMod;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
+import net.minecraft.item.ItemGroup; // 這是從 Minecraft 導入的 ItemGroups 類。此類包含 Minecraft 中的所有默認項目組。
+import net.minecraft.item.ItemStack; // 這是從 Minecraft 導入的 ItemStack 類。此類表示 Minecraft 中物品的堆棧。
+import net.minecraft.item.Items; // 這是從 Minecraft 導入的 Items 類。此類包含 Minecraft 中的所有默認物品。
+import net.minecraft.registry.Registries; // 這是從 Minecraft 導入的 Registries 類。此類提供對遊戲中所有註冊表的訪問，包括物品註冊表。
+import net.minecraft.registry.Registry; // 這是從 Minecraft 導入的 Registry 類。此類表示一個包含物品等對象的註冊表。
+import net.minecraft.text.Text; // 這是從 Minecraft 導入的 Text 類。此類表示 Minecraft 中的文字。
+import net.minecraft.util.Identifier; // 這是從 Minecraft 導入的 Identifier 類。此類表示 Minecraft 對象的唯一標識符。
 
 public class ModItemGroups {
     // 設定 群組有哪些物品
@@ -33,34 +33,45 @@ public class ModItemGroups {
     }
 }
 
-
 // src\main\java\net\kaupenjoe\tutorialmod\item\ModItems.java
 package net.kaupenjoe.tutorialmod.item;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;  // 這是從 Fabric API 導入的 FabricItemSettings 類。此類用於配置 Fabric 模組中創建的新項目的設置。
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries; // 這是從 Fabric API 導入的 FabricItemGroupEntries 類。此類用於將新項目註冊到現有的項目組。
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents; // 這是從 Fabric API 導入的 ItemGroupEvents 類。此類提供事件，可用於修改項目組的行為。
 import net.kaupenjoe.tutorialmod.TutorialMod;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
+import net.minecraft.item.Item; // 這是從 Minecraft 導入的 Item 類。此類表示 Minecraft 中的物品。
+import net.minecraft.item.ItemGroups; // 這是從 Minecraft 導入的 ItemGroups 類。此類包含 Minecraft 中的所有默認項目組。
+import net.minecraft.registry.Registries; // 這是從 Minecraft 導入的 Registries 類。此類提供對遊戲中所有註冊表的訪問，包括物品註冊表。
+import net.minecraft.registry.Registry; // 這是從 Minecraft 導入的 Registry 類。此類表示一個包含物品等對象的註冊表。
+import net.minecraft.util.Identifier; // 這是從 Minecraft 導入的 Identifier 類。此類表示 Minecraft 對象的唯一標識符。
 
+
+//此行宣告了一個名為 ModItems 的類別。此類別包含了註冊模組物品的程式碼。
 public class ModItems {
 
+    //此兩行註冊了兩個新的物品：Ruby 和 Raw Ruby。
+    //registerItem() 函式接受兩個參數：物品名稱和物品設定。
+    //物品設定用於指定物品的屬性，例如堆疊大小、材質、創意模式分類等。
     public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings())); //註冊 Ruby
     public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new FabricItemSettings())); //註冊 Raw Ruby
 
-    // 將 RUBY RAW RUBY 加入 Ingredient Tab 群組
+    // 此函式將 RUBY 和 RAW_RUBY 物品加入到 Ingredient Tab 群組中。
+    // Ingredient Tab 群組是 Minecraft 預設的創意模式分類之一，用於存放所有與製作相關的物品。
     private static void addItemsToIngredientTabItemGroup(FabricItemGroupEntries entries){
         entries.add(RUBY);
         entries.add(RAW_RUBY);
     }
 
+    // 此函式用於註冊新的物品。
+    // Registry.register() 函式將物品添加到 Minecraft 的物品註冊表中。
     public static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
     }
 
+
+    // 此函式用于註冊所有模組的物品。
+    // 它會先註冊 RUBY 和 RAW_RUBY 物品，然後將它們加入到 Ingredient Tab 群組中。
+    // 總而言之，ModItems 類別用於註冊模組的物品並將它們加入到創意模式分類中。
     public static void registerModItems(){
         TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientTabItemGroup);
@@ -68,28 +79,35 @@ public class ModItems {
     }
 }
 
+// 請協助說明下面 java 每一行的用途
+// TutorialMod 類別是模組的主類別，它負責註冊模組的物品類別和物品。
 // src\main\java\net\kaupenjoe\tutorialmod\TutorialMod.java
+
 package net.kaupenjoe.tutorialmod;
+import net.fabricmc.api.ModInitializer; //此行導入 Fabric Mod 程式庫的 ModInitializer 介面。ModInitializer 介面定義了一個 onInitialize() 方法，用於在模組載入時初始化模組。
+import net.kaupenjoe.tutorialmod.item.ModItemGroups; //此行導入了模組的 物品註冊類別。 
+import net.kaupenjoe.tutorialmod.item.ModItems;      //此行導入了模組的 物品類別。
 
-import net.fabricmc.api.ModInitializer;
-
-import net.kaupenjoe.tutorialmod.item.ModItemGroups;
-import net.kaupenjoe.tutorialmod.item.ModItems;
+// 此行導入了 slf4j 程式庫的 Logger 和 LoggerFactory 類別。slf4j 是一個用於記錄日誌的程式庫。
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// 此行宣告了一個名為 TutorialMod 的類別。此類別繼承了 ModInitializer 介面，並實作了 onInitialize() 方法。
 public class TutorialMod implements ModInitializer {
     
-    //設定 MOD_ID
+    // 此兩行宣告了模組的 ID 和日誌記錄器。模組 ID 用於指定模組的名稱，日誌記錄器用於記錄模組的日誌。
 	public static final String MOD_ID = "tutorialmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    // 此方法是 ModInitializer 介面的預設方法。它將在模組載入時被呼叫。
+    // 此方法中呼叫了 ModItemGroups.registerItemGroups() 和 ModItems.registerModItems() 方法來註冊模組的物品類別和物品。
 	@Override
 	public void onInitialize() {
 		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 	}
 }
+
 
 // src\main\java\net\kaupenjoe\tutorialmod\TutorialModClient.java
 package net.kaupenjoe.tutorialmod;
